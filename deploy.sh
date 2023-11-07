56c790652326a1e14af7ff30c45e528649cef875
vedansh-377 <110664790+vedansh-377@users.noreply.github.com>
2023-11-07 16:13:37 +0530

    second run

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
 # Set up AWS credentials using environment variables
-export AWS_ACCESS_KEY_ID=your_access_key
-export AWS_SECRET_ACCESS_KEY=your_secret_key
+export AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY
+
 export AWS_DEFAULT_REGION=us-west-1
 
 # Delete S3 Bucket
-aws s3 rb s3://vedansh --force || true  # Ignore error if bucket doesn't exist
+aws s3 rb s3://vedansh --force  # Ignore error if bucket doesn't exist
 
 # Deploy S3 Bucket
 aws s3api create-bucket --bucket vedansh --region us-west-1 --create-bucket-configuration LocationConstraint=us-west-1
 
-# Rest of your script...
+# Upload Image to S3
+aws s3 cp WhiteSaucePasta.jpg s3://vedansh
+
+# Delete Lambda Function
+aws lambda delete-function --function-name vedu --region eu-west-1  # Ignore error if function doesn't exist
+
+# Deploy Lambda Function
+aws lambda create-function --function-name vedu --runtime python3.11 --role arn:aws:iam::115941228118:role/lambda --handler lambda_function.lambda_handler --zip-file fileb://PastaOrderFunction-195f7a38-883c-43a0-a8f3-007559e8df72.zip --region eu-west-1
+
+# Export and Store Lex Bot as Artifact
+aws lex-models get-export --name "BookTrip_enIN" --export-type "LEX" --resource-type "BOT" --resource-version "1" --output json --no-cli-pager > exported_bot.json
+
+# Download Lex Bot Data
+curl -o exported_bot.zip "URL_PROVIDED_IN_JSON"
+
+# List Downloaded Files
+ls -l
+
+# Save Exported Bot as Artifact
+# (The section for uploading artifacts may need to be handled separately based on your environment and needs.)
